1341e327cf94296033b3c3f9e0ffddcaaecee673
Yomaira Escano <yjescano@gmail.com>
2018-04-10 02:05:29 +0000

    automatically backed up by learn

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,25 @@
+class RecipesController < ApplicationController
+  def new
+    @recipe = Recipe.new
+    # @recipes = @recipe.ingredients.build
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @recipe = Recipe.find_by(params[:id])
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :ingredients_ids => [], ingredients_attributes: [:id, :name])
+  end
+end
